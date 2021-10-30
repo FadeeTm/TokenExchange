475958f1810ce7c2a203dec826eba84008347f5f
Denis Leznevskiy <leznevskiy@mail.ru>
2021-10-31 02:23:09 +0700

    features/add new markets

--- cex.py
+++ cex.py
@@ -4,33 +4,6 @@ import ccxt
 import cbpro
 import gate_api
 
-'''
-def coinbase1(token1, token2):
-    client = cbpro.PublicClient()
-    auth_client = cbpro.AuthenticatedClient(coinbase_api_key, coinbase_api_secret, "1erryCV7peJtwPI2")
-
-    ob = auth_client.get_product_order_book(product_id=token1+"-"+token2,level=2)
-
-    return ob
-'''
-
-def gateio(token1, token2):
-    configuration = gate_api.Configuration(
-        host="https://api.gateio.ws/api/v4"
-    )
-    client = gate_api.ApiClient(configuration)
-    instance = gate_api.SpotApi(client)
-    order_book = instance.list_order_book(currency_pair=token1+"_"+token2)
-    price = instance.list_tickers(currency_pair=token1+"_"+token2)[0].last
-    return order_book, price
-
-
-def ftx(token1, token2):
-    ftx = ccxt.binance()
-    order_book = ftx.fetch_order_book(token1 + "/" + token2)
-    tickers = ftx.fetch_tickers(token1 + "/" + token2)
-    return order_book, tickers[token1 + "/" + token2]['last']
-
 
 def binance(token1, token2):
     client = Client(binance_api_key, binance_api_secret)
@@ -43,18 +16,69 @@ def binance(token1, token2):
     return order_book, result['price']
 
 
+def coinbase1(token1, token2):
+    client = cbpro.PublicClient()
+    auth_client = cbpro.AuthenticatedClient(coinbase_api_key, coinbase_api_secret, "1erryCV7peJtwPI2")
+
+    ob = auth_client.get_product_order_book(product_id=token1+"-"+token2,level=2)
+
+    return ob
+
+
+def ftx(token1, token2):
+    ftx = ccxt.binance()
+    order_book = ftx.fetch_order_book(token1 + "/" + token2)
+    tickers = ftx.fetch_tickers(token1 + "/" + token2)
+    return order_book, tickers[token1 + "/" + token2]['last']
+
+
+def kucoin1(token1, token2):
+    kucoin_ = ccxt.kucoin(coinb_apiKey, coin_secret, coinb_password)
+    fetch_ord = kucoin_.fetch_order_book(token1 + '/' + token2) 
+    fetch_tick = kucoin_.fetch_ticker(token1 + '/' + token2)['info']['last']
+    return(fetch_ord, fetch_tick)
+
+
+def gateio(token1, token2):
+    configuration = gate_api.Configuration(
+        host="https://api.gateio.ws/api/v4"
+    )
+    client = gate_api.ApiClient(configuration)
+    instance = gate_api.SpotApi(client)
+    order_book = instance.list_order_book(currency_pair=token1+"_"+token2)
+    price = instance.list_tickers(currency_pair=token1+"_"+token2)[0].last
+    return order_book, price
+
+
+def kraken1(token1, token2):
+    kraken = ccxt.kraken(kraken_apiKey, kraken_secret)
+    fetch_ord = kraken.fetch_order_book(token1 + '/' + token2) 
+    fetch_tick = kraken.fetch_ticker(token1 + '/' + token2)['last']
+    return(fetch_ord, fetch_tick)
+
+
+def huobi_(token1, token2):
+    huobi = ccxt.huobi(huobi_apiKey, huobi_secret)
+    fetch_ord = huobi.fetch_order_book(token1 + '/' + token2) 
+    fetch_tick = huobi.fetch_ticker(token1 + '/' + token2)['last']
+    return(fetch_ord, fetch_tick)
+
+
 def main():
-    token1 = "BNB"
-    token2 = "BTC"
+    token1 = "BTC"
+    token2 = "USDT"
 
-    print(binance(token1, token2)[0])
-    print(binance(token1, token2)[1])
+    # print(kucoin1(token1, token2)[0])
+    # print(kucoin1(token1, token2)[1])
 
-    print(ftx(token1, token2)[0])
-    print(ftx(token1, token2)[1])
+    # print(binance(token1, token2)[0])
+    # print(binance(token1, token2)[1])
 
-    print(gateio(token1, token2)[0])
-    print(gateio(token1, token2)[1])
+    # print(ftx(token1, token2)[0])
+    # print(ftx(token1, token2)[1])
+
+    # print(gateio(token1, token2)[0])
+    # print(gateio(token1, token2)[1])
 
 
 if __name__ == "__main__":
--- example_config.py
+++ example_config.py
@@ -1,8 +1,22 @@
-binance_api_key = ''
-binance_api_secret = ''
+#kucoin1_keys
+coinb_apiKey = ''
+coin_secret = ''
+coinb_password = ''
 
-ftx_api_key = ''
-ftx_api_secret = ''
+#kraken1
+kraken_apiKey = ''
+kraken_secret = ''
 
+#huobi_
+huobi_apiKey = ''
+huobi_secret = ''
+
+# binance keys
+binance_api_key = ""
+binance_api_secret = ""
+# ftx keys
+ftx_api_key = ""
+ftx_api_secret = ""
+# coinbase keys
 coinbase_api_key = ''
 coinbase_api_secret = ''
