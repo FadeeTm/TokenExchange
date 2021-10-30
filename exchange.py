bc5c6c187672bc08025d142236243f8911300c90
ipu_ <uwu@ipu.moe>
2021-10-31 05:14:37 +0700

    feature/calculation on merged orderbooks

--- exchange.py
+++ exchange.py
@@ -2,30 +2,66 @@ def get_orderbook_sum(orderbook, quantity):
     cur_q = quantity
     sum = .0
     for item in orderbook:
-        if cur_q > float(item[1]):
-            cur_q -= float(item[1])
-            sum += float(item[1]) * float(item[0])
+        if cur_q > item[1]:
+            cur_q -= item[1]
+            sum += item[1] * item[0]
         else:
-            sum += cur_q * float(item[0])
-            break
-    return sum
+            sum += cur_q * item[0]
+            return sum
+    return -1
+    
 
-def get_max_discount_sell(token1, token2, quantity):
-    orderbook, price = cex.binance(token1, token2)
-    orderbook_sum = get_orderbook_sum(orderbook['bids'], quantity)
-    nominal = float(price) * quantity
+def get_max_discount_sell(orderbook, price, quantity):
+    orderbook_sum = get_orderbook_sum(orderbook, quantity)
+    if orderbook_sum == -1:
+        print('error1')
+    nominal = price * quantity
     return 1-orderbook_sum/nominal
 
-def get_max_discount_buy(token1, token2, quantity):
-    orderbook, price = cex.binance(token1, token2)
-    orderbook_sum = get_orderbook_sum(orderbook['asks'], quantity)
-    nominal = float(price) * quantity
-    return orderbook_sum/nominal
+def get_max_discount_buy(orderbook, price, quantity):
+    orderbook_sum = get_orderbook_sum(orderbook, quantity)
+    if orderbook_sum == -1:
+        print('error2')
+    nominal = price * quantity
+    return 1 - nominal/orderbook_sum
+
+def merge_orderbooks(token1, token2, ifSell = True):
+    funcs = [cex.ftx, cex.ftx]
+    merged_orderbooks = []
+    merged_prices = []
+    for f in funcs:
+        try:
+            a = f(token1, token2)
+            if ifSell:
+                merged_orderbooks += a[0]['bids']
+            else:
+                merged_orderbooks += a[0]['asks']
+            merged_prices.append(a[1])
+        except Exception as e:
+            print(e)
+    if ifSell:
+        merged_orderbooks.sort(reverse=True)
+    else:
+        merged_orderbooks.sort(reverse=False)
+    sum = 0
+    for price in merged_prices:
+        sum+=price
+    price = sum/len(merged_prices)
+    return merged_orderbooks, price
+
+
 
 if __name__ == "__main__":
     import cex
     token1 = "BTC"
     token2 = "USDT"
     quantity = 100
-    disc = get_max_discount_buy(token1, token2, quantity)
-    print(disc)
+    
+    a = merge_orderbooks(token1, token2, ifSell = False)
+    b = merge_orderbooks(token1, token2, ifSell = True)
+    disc_s = get_max_discount_sell(a[0], a[1], quantity)
+    disc_b = get_max_discount_buy(b[0], b[1], quantity)
+
+    print(f'sell: {disc_s}')
+    print(f'buy: {disc_b}')
+    
