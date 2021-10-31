2eb5629e5486e428979d4ef6be617060c6e4fa10
ipu_ <uwu@ipu.moe>
2021-10-31 17:01:08 +0700

    f/add serumDex

--- DEX.py
+++ DEX.py
@@ -12,5 +12,15 @@ def dyDx(token1,token2):
     return(ordB,basePrice)
 
 
+def serumDex(token1,token2):
+    rOrdBook = requests.get(f'https://serum-api.bonfida.com/orderbooks/{token1}{token2}')
+    jOrdBook = rOrdBook.json()["data"]
+    ordB = {}
+    ordB['bids'] = [[float(item['price']), float(item['size'])] for item in jOrdBook['bids']] 
+    ordB['asks'] = [[float(item['price']), float(item['size'])] for item in jOrdBook['asks']]
+    basePrice = (ordB['bids'][0][0] + ordB['asks'][0][0]) / 2
+    return(ordB,basePrice)
+
+
 if __name__ == '__main__':
-    print(dyDx('UNI','USD'))
+    print(serumDex('SUSHI','USDT'))
